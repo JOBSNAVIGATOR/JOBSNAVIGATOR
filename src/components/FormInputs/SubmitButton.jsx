779c09c363a157f3167d903b884e055c49a0aee8
Ujjwal <ujjwaljindal835@gmail.com>
2024-07-19 20:25:58 +0530

    made the bottom gradient a separate component

--- src/components/FormInputs/SubmitButton.jsx
+++ src/components/FormInputs/SubmitButton.jsx
@@ -1,5 +1,6 @@
 import { Plus } from "lucide-react";
 import React from "react";
+import { BottomGradient } from "../ui/BottomGradient";
 
 export default function SubmitButton({
   isLoading = false,
@@ -45,12 +46,3 @@ export default function SubmitButton({
     </div>
   );
 }
-
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
-    </>
-  );
-};
