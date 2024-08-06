24acd0eb1efb098e84711a2cc62bffa29d6d7024
Ujjwal <ujjwaljindal835@gmail.com>
2024-08-06 15:59:35 +0530

    created candidates section table i dashboard

--- src/app/(back-office)/dashboard/candidates/page.jsx
+++ src/app/(back-office)/dashboard/candidates/page.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { candidates } from "@/data";
+import DataTable from "@/components/data-table-components/DataTable";
+import { columns } from "./columns";
+import Heading from "@/components/backOffice/Heading";
 
 export default function page() {
+  // const jobs = await getData("jobs");
   return (
     <div>
-      <h2>Candidates</h2>
+      <div className="mt-4 py-4">
+        {/* Header */}
+        <Heading title="Candidates" />
+      </div>
+
+      {/* table */}
+      <div className="py-8">
+        <DataTable data={candidates} columns={columns} />
+      </div>
     </div>
   );
 }
