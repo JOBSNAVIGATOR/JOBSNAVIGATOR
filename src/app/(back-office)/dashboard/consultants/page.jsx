91ead85c52b81deb1d8818cedf862e22fa78bb33
Ujjwal <ujjwaljindal835@gmail.com>
2024-08-06 16:06:47 +0530

    created structure for the consultants in dashboard for admin

--- src/app/(back-office)/dashboard/consultants/page.jsx
+++ src/app/(back-office)/dashboard/consultants/page.jsx
@@ -1,9 +1,24 @@
+import PageHeader from "@/components/backOffice/PageHeader";
 import React from "react";
+import { jobs } from "@/data";
+import DataTable from "@/components/data-table-components/DataTable";
+import { columns } from "./columns";
 
 export default function page() {
+  // const jobs = await getData("jobs");
   return (
     <div>
-      <h2>Consultant</h2>
+      {/* Header */}
+      <PageHeader
+        heading={"Consultants"}
+        href={"/dashboard/consultant/new"}
+        linkTitle={"Add Consultant"}
+      />
+
+      {/* table */}
+      <div className="py-8">
+        <DataTable data={jobs} columns={columns} />
+      </div>
     </div>
   );
 }
