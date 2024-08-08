aa768fd0f9cf38de69ed6180d4caeac06da5ab2c
Ujjwal <ujjwaljindal835@gmail.com>
2024-08-08 16:20:59 +0530

    created downloadexcel component and used it in the jobs section

--- src/app/(back-office)/dashboard/jobs/page.jsx
+++ src/app/(back-office)/dashboard/jobs/page.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { jobs } from "@/data";
 import DataTable from "@/components/data-table-components/DataTable";
 import { columns } from "./columns";
+import DownloadExcel from "@/components/backOffice/DownloadExcel";
 
 export default function page() {
   // const jobs = await getData("jobs");
@@ -14,6 +15,9 @@ export default function page() {
         href={"/dashboard/jobs/new"}
         linkTitle={"Post Job"}
       />
+      <div className="flex justify-end">
+        <DownloadExcel data={jobs} fileName="jobs.xlsx" />
+      </div>
 
       {/* table */}
       <div className="py-8">
