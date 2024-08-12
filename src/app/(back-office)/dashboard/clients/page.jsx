5ad4e826d515c86123b92a7a768f57c82b987b63
Ujjwal <ujjwaljindal835@gmail.com>
2024-08-12 23:00:14 +0530

    added download excel functionality in the clients section

--- src/app/(back-office)/dashboard/clients/page.jsx
+++ src/app/(back-office)/dashboard/clients/page.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { consultants } from "@/data";
 import DataTable from "@/components/data-table-components/DataTable";
 import { columns } from "./columns";
+import DownloadExcel from "@/components/backOffice/DownloadExcel";
 
 export default function page() {
   // const jobs = await getData("jobs");
@@ -14,6 +15,10 @@ export default function page() {
         href={"/dashboard/clients/new"}
         linkTitle={"Add Client"}
       />
+
+      <div className="flex justify-end">
+        <DownloadExcel data={consultants} fileName="clients.xlsx" />
+      </div>
 
       {/* table */}
       <div className="py-8">
