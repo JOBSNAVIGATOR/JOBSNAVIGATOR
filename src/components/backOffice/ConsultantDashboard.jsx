7ed8a1eee7a1816a10e2c32f621c3f37b35eef7b
Ujjwal <ujjwaljindal835@gmail.com>
2024-11-02 21:31:09 +0530

    added some charts for consultant Dashboard

--- src/components/backOffice/ConsultantDashboard.jsx
+++ src/components/backOffice/ConsultantDashboard.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import DashboardChart from "./DashboardChart";
 
 export default function ConsultantDashboard() {
-  return <div>ConsultantDashboard</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
+      <DashboardChart />
+      <DashboardChart />
+      <DashboardChart />
+      <DashboardChart />
+    </div>
+  );
 }
