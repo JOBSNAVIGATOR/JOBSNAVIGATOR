ef16910fae1eecb24d4ec703a13bb86acf45b3c7
Ujjwal <ujjwaljindal835@gmail.com>
2024-11-03 23:43:51 +0530

    modified the dashboard chart

--- src/components/backOffice/DashboardChart.jsx
+++ src/components/backOffice/DashboardChart.jsx
@@ -63,8 +63,8 @@ export default function DashboardChart() {
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>Stats for this month</CardTitle>
+        <CardDescription>August - November 2024</CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
@@ -117,11 +117,11 @@ export default function DashboardChart() {
         </ChartContainer>
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm">
-        <div className="flex items-center gap-2 font-medium leading-none">
+        {/* <div className="flex items-center gap-2 font-medium leading-none">
           Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
+        </div> */}
         <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
+          Showing total Candidates for the last 6 months
         </div>
       </CardFooter>
     </Card>
